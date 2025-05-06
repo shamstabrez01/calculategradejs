3f7be809b947f0229e1d2cb1ae1aa97417a8613f
Shams <shamstabrez1183@gmail.com>
2025-05-06 16:33:39 +0530

    new file

---
+++ count.js
@@ -0,0 +1,11 @@
+for (let count = 0; count <= 100; count++) {
+  console.log("shamstabrez");
+}
+
+console.log("loop is ended");
+
+for (let count = 0; count <= 50; count++) {
+  console.log("i love you");
+}
+
+console.log("loop end");
