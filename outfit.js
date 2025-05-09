5c27ef0e0b70422ec179564b62c89794183daf3d
Shams <shamstabrez1183@gmail.com>
2025-05-09 18:17:34 +0530

    outfit changes

---
+++ outfit.js
@@ -0,0 +1,11 @@
+let weather = cloudy;
+
+if (weather === rainy) {
+  console.log(`carrying umbrella`);
+} else if (weather === cloudy) {
+  console.log(`wear jacket`);
+} else if ("weather === sunny") {
+  console.log(`enjoy the sunny weather`);
+}
+
+console.log("weather === cloudy");
